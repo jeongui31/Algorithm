a3e3ebb22c9a925cc2c3c12c02fbababa8075398
jeongui31 <jangjung31@dgu.ac.kr>
2024-09-05 11:44:23 +0900

    240905

---
+++ queue/1158.py
@@ -0,0 +1,11 @@
+from collections import deque
+N, K = map(int, input().split())
+queue = deque(range(1, N+1))
+result = []
+
+while queue:
+    for _ in range(K-1):
+        queue.append(queue.popleft())
+    result.append(queue.popleft())
+
+print(str(result).replace('[', '<').replace(']', '>'))
