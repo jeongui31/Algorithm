401ef8a51927f18d495ff9510f4b442749a71d86
jeongui31 <jangjung31@dgu.ac.kr>
2024-09-20 16:59:27 +0900

    240920

---
+++ math/9613.py
@@ -0,0 +1,7 @@
+# t = int(input())
+
+# for _ in range(t):
+#     nums = list(map(int, input().split()))
+#     for i in range(1,len(nums)):
+#         while nums[i+1]!=0:
+#             nums[i]=nums[i]//nums[i+1]
